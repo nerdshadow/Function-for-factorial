c1e8869bbed3d95c9eeaa9a1f74f274071e238bc
Evilnood <evilnood666@gmail.com>
2021-03-12 11:37:55 +0200

    Добавьте файлы проекта.

---
+++ Function for factorial.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+long buff = 0;
+long factorial(long x)
+{
+	if (x>1)
+	{
+		buff = x * factorial(x - 1);
+		return buff;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int main()
+{
+	cout << "Choose number for factorialing ;): ";
+	long a;
+	cin >> a;
+	cout << endl << "Factorial of " << a << " is: " << factorial(a);
+
+}
